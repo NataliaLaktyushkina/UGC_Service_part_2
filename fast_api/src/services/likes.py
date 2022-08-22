1e97e71d9e2201a3dcc029135f5e73fe78f36526
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-08-23 01:21:38 +0300

    changes in api service

--- fast_api/src/services/likes.py
+++ fast_api/src/services/likes.py
@@ -1,28 +1,30 @@
 from fastapi import Depends
-from services.service import AbstractDB, MongoDB
+from services.service import AbstractLikeDB
 from db.mongo_db import get_mongo
+from models.likes import LikeAdded, LikeDeleted, LikeUpdated, MovieRating
 
 
 class LikesHandler:
-    def __init__(self, likes_db: AbstractDB):
+    def __init__(self, likes_db: AbstractLikeDB):
         self.likes_db = likes_db
 
-    async def add_like(self, movie_id: str, user_id: str) -> BookmarkAdded:
-        bookmark_added = await self.bookmark_db.add_bookmark(movie_id, user_id)
-        return BookmarkAdded(added=bookmark_added)
+    async def add_like(self, movie_id: str, user_id: str) -> LikeAdded:
+        like_added = await self.likes_db.add_like(movie_id, user_id)
+        return LikeAdded(added=like_added)
 
-    async def get_movie_rating(self, movie_id: str) -> BookmarksList:
-        return await self.bookmark_db.get_bookmarks_list(user_id)
+    async def get_movie_rating(self, movie_id: str) -> MovieRating:
+        return await self.likes_db.get_movie_rating(movie_id)
 
-    async def delete_like(self, movie_id: str, user_id: str) -> BookmarkDeleted:
-        bookmark_deleted = await self.bookmark_db.delete_bookmark(movie_id, user_id)
-        return BookmarkDeleted(deleted=bookmark_deleted)
+    async def delete_like(self, movie_id: str, user_id: str) -> LikeDeleted:
+        like_deleted = await self.likes_db.delete_like(movie_id, user_id)
+        return LikeDeleted(deleted=like_deleted)
 
-    async def update_like(self, movie_id: str, user_id: str):
-       pass
+    async def update_like(self, movie_id: str, user_id: str) -> LikeUpdated:
+        like_updated = await self.likes_db.update_like(movie_id, user_id)
+        return LikeUpdated(updated=like_updated)
 
 
 def get_db(
-        likes_db: AbstractDB = Depends(get_mongo)
+        likes_db: AbstractLikeDB = Depends(get_mongo)
 ) -> LikesHandler:
-    return LikesHandler(MongoDB(likes_db))
+    return LikesHandler(AbstractLikeDB(likes_db))
--- fast_api/src/services/service.py
+++ fast_api/src/services/service.py
@@ -1,11 +1,17 @@
 import abc
+
 from models.bookmarks import BookmarksList
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.responses import JSONResponse
 from typing import Union
+from models.likes import MovieRating
 
 
-class AbstractDB(abc.ABC):
+# class AbstractDB(abc.ABC):
+#     pass
+
+
+class AbstractBookmarkDB(abc.ABC):
 
     @abc.abstractmethod
     def add_bookmark(self, movie_id: str, user_id: str) -> bool:
@@ -20,31 +26,39 @@ class AbstractDB(abc.ABC):
         pass
 
 
-class MongoDB(AbstractDB):
+class AbstractLikeDB(abc.ABC):
+    @abc.abstractmethod
+    def add_like(self, movie_id: str, user_id: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def get_movie_rating(self, user_id: str) -> MovieRating:
+        pass
+
+    @abc.abstractmethod
+    def delete_like(self, movie_id: str, user_id: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def update_like(self, movie_id: str, user_id: str) -> bool:
+        pass
+
+
+# class MongoDB(AbstractDB):
+#
+#     def __init__(self, client: AsyncIOMotorClient):
+#         self.client = client
+#         self.ugc_db = self.client.ugc_database
+        # self.bookmarks_collection = self.ugc_db["bookmarks"]
+        # self.likes_collection = self.ugc_db["scores"]
+
+
+class MongoDBBookmark(AbstractBookmarkDB):
+
     def __init__(self, client: AsyncIOMotorClient):
         self.client = client
         self.ugc_db = self.client.ugc_database
         self.bookmarks_collection = self.ugc_db["bookmarks"]
-
-    async def get_bookmarks_list(self, user_id: str) -> BookmarksList:
-        document = await self.bookmarks_collection.find_one({"user_id": user_id})
-        movies = document["movie_id"]
-        return movies
-
-    async def delete_bookmark(self, movie_id: str, user_id: str) -> Union[bool, JSONResponse]:
-        doc = await self.bookmarks_collection.find_one({"user_id": user_id})
-        if doc is None:
-            return JSONResponse(content="Movie id was not found")
-        else:
-            doc_id = doc["_id"]
-            result = await self.bookmarks_collection.update_one(
-                {"_id": doc_id},
-                {"$pull":
-                     {"movie_id": movie_id}
-                 })
-
-            if result.modified_count:
-                return True
 
     async def add_bookmark(self, movie_id: str, user_id: str) -> bool:
         """Add bookmark to Mongo DB"""
@@ -67,10 +81,30 @@ class MongoDB(AbstractDB):
             result = await self.bookmarks_collection.update_one(
                 {"_id": doc_id},
                 {"$push":
-                     {"movie_id": movie_id}
+                    {"movie_id": movie_id}
                  })
 
             if result.modified_count:
                 return True
 
         return False
+
+    async def get_bookmarks_list(self, user_id: str) -> BookmarksList:
+        document = await self.bookmarks_collection.find_one({"user_id": user_id})
+        movies = document["movie_id"]
+        return movies
+
+    async def delete_bookmark(self, movie_id: str, user_id: str) -> Union[bool, JSONResponse]:
+        doc = await self.bookmarks_collection.find_one({"user_id": user_id})
+        if doc is None:
+            return JSONResponse(content="Movie id was not found")
+        else:
+            doc_id = doc["_id"]
+            result = await self.bookmarks_collection.update_one(
+                {"_id": doc_id},
+                {"$pull":
+                 {"movie_id": movie_id}
+                 })
+
+            if result.modified_count:
+                return True
